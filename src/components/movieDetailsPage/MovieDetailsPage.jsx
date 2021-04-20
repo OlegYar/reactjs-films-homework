5a93241ccc81f0a1acafd44c05f50b00fdc38108
Oleg <oleg.yarotskiy.99@gmail.com>
2021-04-20 14:42:29 +0300

    added header

--- src/components/movieDetailsPage/MovieDetailsPage.jsx
+++ src/components/movieDetailsPage/MovieDetailsPage.jsx
@@ -3,14 +3,12 @@ import Search from '../search';
 import './MovieDetailsPage.scss';
 
 const MovieDetailsPage = () => (
-  <header>
-    <div className="wrapper">
-      <div className="search-line">
-        <h1>FILM</h1>
-        <Search />
-      </div>
+  <div className="wrapper">
+    <div className="header">
+      <span className="logo">FILMS</span>
+      <Search />
     </div>
-  </header>
+  </div>
 );
 
 export default MovieDetailsPage;
