0982cf0d9e9da00f2380657a67aee2d655bee775
Oleg <oleg.yarotskiy.99@gmail.com>
2021-08-26 17:29:13 +0300

    refactoring in MovieList

--- src/components/movieList/MovieList.jsx
+++ src/components/movieList/MovieList.jsx
@@ -6,15 +6,13 @@ import Spinner from '../spinner/Spinner';
 import styles from './MovieList.module.scss';
 import { loadingFilmsSelector } from '../../modules/selectors';
 
-const MovieList = ({ films, genres }) => {
+const MovieList = ({ films = [], genres }) => {
   const isFilmLoaded = useSelector(loadingFilmsSelector);
-  const movieCards = films
-    ? films.map((movie) => (
-      <li key={movie.id} className={styles.movieListItem}>
-        <MovieListItem movie={movie} genres={genres} />
-      </li>
-    ))
-    : null;
+  const movieCards = films.map((movie) => (
+    <li key={movie.id} className={styles.movieListItem}>
+      <MovieListItem movie={movie} genres={genres} />
+    </li>
+  ));
   const spinner = isFilmLoaded ? <Spinner /> : null;
   return (
     <div className={styles.movieListContainer}>
