3b2100f3883c77175e514e43234365383e3d4375
Oleg <oleg.yarotskiy.99@gmail.com>
2021-08-30 16:44:03 +0300

    search in storybook fixed

--- src/stories/Search.stories.jsx
+++ src/stories/Search.stories.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Search from '../components/search/Search';
+import store from '../store';
 
 export default {
   title: 'Part_2/Search',
@@ -9,11 +11,11 @@ export default {
     backgroundColor: { control: 'color' },
   },
 };
-
 // eslint-disable-next-line react/jsx-props-no-spreading
-const Template = (args) => <Search {...args} />;
+const Template = (args) => <Provider store={store}><Router><Search {...args} /></Router></Provider>;
 
 export const Primary = Template.bind({});
-Primary.args = {
 
+Primary.parameters = {
+  backgrounds: { default: 'black' },
 };
