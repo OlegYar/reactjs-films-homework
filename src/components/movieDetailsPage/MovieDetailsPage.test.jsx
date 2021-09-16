6868a4f7dbe4d7b6cf05cc9f30d9a8d833694ea8
Oleg <oleg.yarotskiy.99@gmail.com>
2021-09-16 19:15:23 +0300

    added test coverage

--- src/components/movieDetailsPage/MovieDetailsPage.test.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '../../utils/test-utils';
-import MovieDetailsPage from './MovieDetailsPage';
-
-test('render MovieDetailsPage', () => {
-  render(<MovieDetailsPage />);
-});
--- src/components/videoWindow/VideoWindow.test.jsx
+++ src/components/videoWindow/VideoWindow.test.jsx
@@ -3,7 +3,7 @@ import { render, screen } from '../../utils/test-utils';
 import VideoWindow from './VideoWindow';
  
 test('renders VideoWindow component', () => {
-  render(<VideoWindow />);
+  render(<VideoWindow videoKey='c31KJTzt5YY' />);
  
   expect(screen.getByTitle('YouTube video player')).toBeTruthy();
 });
